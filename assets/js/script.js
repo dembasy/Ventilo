68814e486fbe4c4668046d7e4d017418c707398e
QuentinDll <quentin.delplace13@gmail.com>
2018-07-05 09:31:13 +0200

    ajout bootstrap.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.sidebar-nav').hide();
+});
+
+console.log('event appen');
+
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal("toggle");
+    });
+});
